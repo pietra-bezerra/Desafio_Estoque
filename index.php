40b83c5a54888de789ed322ccb277e8931d5e0eb
matheus-benevides <matheus.g.benevides@aluno.senai.br>
2025-11-04 15:43:57 -0300

    Base Inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ERP</title>
+
+    <!-- Estilização -->
+     <link rel="stylesheet" href="css/style.css">
+
+     <!-- Bootstrap Icons -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+</head>
+<body>
+    
+
+    <!-- JS -->
+    <script src="scripts/script.js"></script>
+</body>
+</html>
